8fb2e9bca8c87e00e9a42371cd87b700d41b907f
Gustav Brodin <brodin.gustav@gmail.com>
2025-02-21 10:08:23 +0100

    Commit - Skapat Databasanslutning via konstruktur och tillhörande tabell i MusicDbContext

---
+++ Data/MusicDbContext.cs
@@ -0,0 +1,16 @@
+using dt191g_mom4.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace dt191g_mom4.Data;
+
+public class MusicDbContext : DbContext 
+{    
+    //Konstruktur
+    public MusicDbContext(DbContextOptions<MusicDbContext> options) : base(options) { }     //Databasanslutning
+
+    //Tabeller
+    public DbSet<Music> Music { get; set; }
+
+}
+
